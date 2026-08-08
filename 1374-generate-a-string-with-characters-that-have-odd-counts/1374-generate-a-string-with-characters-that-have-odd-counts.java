6300950040dde836d76e45f3b760a096d6b3d8c7
Monishwaran <monishwaran077@gmail.com>
2026-08-08 22:48:47 +0530

    Time: 1 ms (97.28%), Space: 42.8 MB (65.4%) - LeetHub

---
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String generateTheString(int n) {
+        StringBuilder s = new StringBuilder();
+        if (n % 2 == 1) {
+            for (int i = 0; i < n; i++) {
+                s.append('a');
+            }
+        } else {
+            for (int i = 0; i < n - 1; i++) {
+                s.append('a');
+            }
+            s.append('b');
+        }
+        return s.toString();
+    }
+}
