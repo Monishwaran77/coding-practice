3c4af31f290b4b4ea4cce0ff71bb0839c9fd46de
Monishwaran <monishwaran077@gmail.com>
2026-08-08 22:49:21 +0530

    Time: 1 ms (97.28%), Space: 42.9 MB (33.51%) - LeetHub

--- 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.java
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.java
@@ -3,13 +3,13 @@ class Solution {
         StringBuilder s = new StringBuilder();
         if (n % 2 == 1) {
             for (int i = 0; i < n; i++) {
-                s.append('a');
+                s.append('x');
             }
         } else {
             for (int i = 0; i < n - 1; i++) {
-                s.append('a');
+                s.append('x');
             }
-            s.append('b');
+            s.append('y');
         }
         return s.toString();
     }
