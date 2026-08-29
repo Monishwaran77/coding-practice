1f546083655cd3db4803821dfdbb91d1ce5ea8d8
Monishwaran <monishwaran077@gmail.com>
2026-08-29 18:38:45 +0530

    Time: 0 ms (100%), Space: 41.8 MB (87.02%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=2){
+            return n;
+        }
+        int step1=1;
+        int step2=2;
+        
+        for(int i=3;i<=n;i++){
+            int now=step2+step1;
+            step1=step2;
+            step2=now;
+        }
+        return step2;
+    }
+}
